5e311963587901ab956e4f8e294f1647bd66f129
Manivannan <a1842066@adelaide.edu.au>
2024-06-06 18:12:01 +0930

    updated url for api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,7 @@ const App = () => {
        const prompt = `${favoriteColor} monochromatic minimalist portrait of a ${favoriteAnimal} head with a random, strong facial expression in a simplistic cartoon style with satirical humor, simple details, and rounded minimalist shapes, wearing ${favoriteFood} as accessories.`;
       
       const response = await axios.post(
-        'http://localhost:5001/generate-profile-picture',
+        'https://profile-picture-server-hmg9i4r3o-beingmanis-projects.vercel.app/1/generate-profile-picture',
         { prompt },
         { responseType: 'arraybuffer' }
       );
